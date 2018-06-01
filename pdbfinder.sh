4ca4de3c41563c6a79c7f7c46ef80629e6e9694b
steven-albanese <steven.albanese91@gmail.com>
2018-06-01 12:33:48 -0400

    changing the shell script

--- pdbfinder.sh
+++ pdbfinder.sh
@@ -4,12 +4,10 @@
 #BSUB -n 1
 #BSUB -R span[ptile=1]
 #BSUB -R rusage[mem=10]
-#BSUB -W 10:00
-#BSUB -o %J.stdout
+#BSUB -W 144:00
+#BSUB -o %J.stout
 #BSUB -eo %J.stderr
 #BSUB -L /bin/bash
+
 cd $LS_SUBCWD
-
-
-# Launch job 
-python PDBfinder.py -l Imatinib --mode LigAll --biological --fix --ph 7.4
+python PDBfinder.py --mode LigAll -l Imatinib --fix --ph 7.4 --biological_unit
